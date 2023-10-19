58fe6f4c8dc93a4298367daf8e43d635935ffd59
HavallonTech <kestercode@gmail.com>
2023-10-19 12:31:37 +0100

    monty

--- montinter.c
+++ montinter.c
@@ -42,10 +42,11 @@ void bcprocess(FILE *bc, stack_t **stack)
 	char *bc_buff = NULL;/*byte code buffer to store content the byte code file*/
 	size_t bc_len = 0;/*byte code length of the instruction per line*/
 /*	ssize_t read;*/
+	ssize_t readline = 0;/*to handle getline size */
 	unsigned int bc_numln = 0;/*bc_numln keeps track of the line instruction*/
 
 /*	read = getline(&bc_buff, &bc_len, bc);*/
-	while ((getline(&bc_buff, &bc_len, bc)) != -1)
+	while ((readline = getline(&bc_buff, &bc_len, bc)) != -1)
 	{
 		bc_numln++;
 		bc_opcode = strtok(bc_buff, DELIM);
@@ -53,8 +54,10 @@ void bcprocess(FILE *bc, stack_t **stack)
 		if (bc_opcode == NULL || bc_opcode[0] == '#')
 			continue;
 		/*printf("This is a string %s\n", bc_opcode);*/
-
-		bc_exe(bc_opcode, bc_numln, stack);
+		if (readline > 0)
+		{
+			bc_exe(bc_opcode, bc_numln, stack);
+		}
 		/*bc_numln++;*/
 	}
 	free(bc_buff);
@@ -78,3 +81,30 @@ void bc_exe(const char *opcode, unsigned int cmd_num, stack_t **stack)
 	/*this runs the arguments of the byte code instruction*/
 	bc_code->f(stack, cmd_num);
 }
+/*
+void bcprocess(FILE *bc, stack_t **stack)
+ 40 {
+ 41         char *bc_opcode;//byte code operation code/
+ 42         char *bc_buff = NULL;//byte code buffer to store content the byte code file
+ 43         size_t bc_len = 0;/*byte code length of the instruction per line
+ 44 /*      ssize_t read;
+ 45         ssize_t readline = 0;/*to handle getline size 
+ 46         unsigned int bc_numln = 0;/*bc_numln keeps tk of the line instruction
+ 47
+ 48 /*      read = getline(&bc_buff, &bc_len, bc);
+ 49         while ((readline = getline(&bc_buff, &bc_len, bc)) != -1)
+ 50         {
+ 51                 bc_numln++;
+ 52                 bc_opcode = strtok(bc_buff, DELIM);
+ 53                 /*this checks if the bc_buff is empty or commented and ignore
+ 54                 if (bc_opcode == NULL || bc_opcode[0] == '#')
+ 55                         continue;
+ 56                 /*printf("This is a string %s\n", bc_opcode);
+ 57                 if (readline > 0)
+ 58                 {
+ 59                         bc_exe(bc_opcode, bc_numln, stack);
+ 60                 }
+ 61                 //bc_numln++;/
+ 62         }
+ 63         free(bc_buff);
+ 64 }*/
